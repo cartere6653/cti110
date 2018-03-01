72a6819014edc3befda247f76480f937fc59965f
cartere6653 <36968456+cartere6653@users.noreply.github.com>
2018-03-01 19:22:07 +0000

    Add files via upload

---
+++ P2HW1_DistanceTraveled_CarterEric.py
@@ -0,0 +1,25 @@
+#CTI-110
+#P2HW1 - Distance Traveled
+#Eric Carter
+#22February 2018
+#
+
+# Varibles to hold distances.
+distance6Hours = 0.0
+distance10Hours = 0.0
+distance15Hours = 0.0
+
+# Constant for the speed.
+Speed = 70
+
+# Calculate the distance the car will travel in
+#6, 10, and 15 hours
+distance6Hours = Speed * 6
+distance10Hours = Speed * 10
+distance15Hours = Speed * 15
+
+# Print the results.
+print ("the car will travel the following distances:")
+print (distance6Hours,"miles in 6 hours.")
+print (distance10Hours,"miles in 10 hours.")
+print (distance15Hours,"miles in 15 hours.")
