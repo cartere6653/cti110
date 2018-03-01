2a29024d1ef93e93903049bb05acadd414289f4b
cartere6653 <36968456+cartere6653@users.noreply.github.com>
2018-03-01 19:21:36 +0000

    Add files via upload

---
+++ P1T1_CarterEric.py
@@ -0,0 +1,12 @@
+# P1T1
+# Hello World
+# Eric Carter
+# 02/08/2018
+
+print ("Hello World! My name is Eric Carter")
+print ("to my left is Curtis Walker,")
+print ("there is no one to my Right,")
+print ("the student behind me is Candanice Kirk,")
+print ("My work was verified by Curtis Walker.")
+
+print
