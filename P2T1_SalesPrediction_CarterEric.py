9b444a4f7ad9a5f1f82cadfedbb7991bf67a7e71
cartere6653 <36968456+cartere6653@users.noreply.github.com>
2018-03-01 19:20:36 +0000

    Add files via upload

---
+++ P2T1_SalesPrediction_CarterEric.py
@@ -0,0 +1,12 @@
+ # CTI-110
+ #p2t1-Sales Prediction
+ #Eric Carter
+ #20 Feburary 2018
+ #
+
+# Get the projected total sales.
+Total_sales=float(input('Enter the projected sales: '))
+# Calculate the Profit as 23 percent of total Sales.
+profit=Total_sales*0.23
+# Display the profit.
+print('profit is $',format(profit, ',.2f'))
